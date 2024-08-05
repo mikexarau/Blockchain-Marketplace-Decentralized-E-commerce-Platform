1c3e786864417f7c7ab58d44f167ef15e2d11e8b
mikexarau <miqxarau@gmail.com>
2024-08-05 13:09:28 +0200

    Fix bug in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -9,24 +9,6 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    // ropsten: {
-    //   provider: function() {
-    //     return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/b0e2c80524ff42f4824795def6423362")
-    //   },
-    //   network_id: 3
-    // },
-    // kovan: {
-    //   provider: function() {
-    //     return new HDWalletProvider(mnemonic, "kovan.infura.io/v3/b0e2c80524ff42f4824795def6423362")
-    //   },
-    //   network_id: 42
-    // },
-    // main: {
-    //   provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/b0e2c80524ff42f4824795def6423362" + process.env.INFURA_API_KEY),
-    //   network_id: 1,
-    //   gas: 3000000,
-    //   gasPrice: 10000000000
-    // }      
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
